feff0172fa870ad8f6ea7657daccdfe784a22b72
Debbie Bourne <dblashrink@hotmail.com>
2016-09-06 21:12:36 -0500

    redid the parts that weren't working correctly

--- scripts/js.js
+++ scripts/js.js
@@ -1,37 +1,36 @@
 "use strict";
-let BattleBotPlayer = require('./player.js');
 let BattleBotClass = require('./robots.js');
 
-//this creates the player and enemy objects
-let player1 = new BattleBotPlayer.Fighter();
-let enemy = new BattleBotPlayer.Fighter();
-console.log(player1);
-console.log(enemy);
-console.log("welcome to BattleDome");
-
-//this tells app what to hide/show at load. originally I set it up to run at load but it would flash the whole page before hiding the others parts of the site, so this setup hides it from the get-go.
-// $(document).ready( () => {
-  $(".welcome-page").show();
+//this creates the player and enemy variables
+let player1;
+let enemy;
+//this tells app what to hide/show at load.
   $(".choose-bot").hide();
   $(".battleground").hide();
   $("#getName").focus();
-// });
 
-//upon click this function grabs player and enemy name and puts it in player1 and enemy object created earlier. if text boxes do not have a name, it prompts for name.
+//upon click this function grabs player and enemy name, chosen class and puts them in battlebot objects. if text boxes do not have a name, it prompts for name.
 $("#name_button").click(function(e) {
   let playerNameInput = $("#getName").val();
-  // console.log(playerNameInput);
-  player1.playerName = playerNameInput;
-  // console.log(player1);
   let enemyNameInput = $("#opponentName").val();
-  // console.log(enemyNameInput);
-  enemy.playerName = enemyNameInput;
-  // console.log(enemy);
   if (playerNameInput === "" || enemyNameInput === "") {
     window.alert("Please enter both your name and the name of your enemy!");
     } else {
-    console.log("you clicked the button ", playerNameInput, "and will fight ", enemyNameInput);
-        transitionToRobotSelect();
+    // console.log(playerNameInput);
+    // player1.playerName = playerNameInput;
+    // console.log(enemyNameInput);
+    // enemy.playerName = enemyNameInput;
+    let playerClassInput = $("#playerClass").val();
+    // console.log(playerClassInput);
+    player1 = new BattleBotClass[playerClassInput]();
+    player1.name = playerNameInput;
+    console.log(player1);
+    let enemyClassInput = $("#enemyClass").val();
+    // console.log(enemyClassInput);
+    enemy = new BattleBotClass[enemyClassInput]();
+    enemy.name = enemyNameInput;
+    console.log(enemy);
+    transitionToBattleground();
     }
 });
 
@@ -43,48 +42,7 @@ function transitionToRobotSelect () {
   $(".battleground").hide();
 }
 
-//these are the event listeners for the player to choose their class. it adds the class to the player object and then transitions calls the function that transitions to battleground. Can we make this more effecient and put an EL on the div then just pull the id of the actual button?
-$("#brownBear").click( () =>{
-  console.log("you clicked brown bear");
-  player1.class = new BattleBotClass.BrownBear();
-  console.log(player1);
-  transitionToBattleground();
-});
-
-$("#polarBear").click( () =>{
-  console.log("you clicked polar bear");
-  player1.class = new BattleBotClass.PolarBear();
-  console.log(player1);
-  transitionToBattleground();
-});
-
-$("#android").click( () =>{
-  console.log("you clicked android");
-  player1.class = new BattleBotClass.Android();
-  console.log(player1);
-  transitionToBattleground();
-});
-
-$("#elf").click( () =>{
-  console.log("you clicked elf");
-  player1.class = new BattleBotClass.Elf();
-  console.log(player1);
-  transitionToBattleground();
-});
-
-$("#blackWidow").click( () =>{
-  console.log("you clicked black widow");
-  player1.class = new BattleBotClass.BlackWidow();
-  console.log(player1);
-  transitionToBattleground();
-});
-
-$("#brownRecluse").click( () =>{
-  console.log("you clicked brown recluse");
-  player1.class = new BattleBotClass.BrownRecluse();
-  console.log(player1);
-  transitionToBattleground();
-});
+//I had a slew of event listeners on buttons but redid it to follow the directions and removed all of the event listeners.
 
 //function to hide/show divs and show fighter's stats in battleground
 function transitionToBattleground () {
@@ -94,9 +52,10 @@ function transitionToBattleground () {
   $(".battleground").show();
   // console.log(player1.toString());
   // console.log(enemy.toString());
-  $("#inputPlayerStats").text(player1.toString());
-  $("#inputEnemyStats").append(enemy.toString());
+  $("#inputPlayerStats").append("<h3>In this corner</h3><p>" + player1.name + " a " + player1.className + " type bot</p><p>" + "with " + player1.health + " hit points and wielding " + player1.weapon + "</p>");
+  $("#inputEnemyStats").append("<h3>In this corner</h3><p>" + enemy.name + " a " + enemy.className + " type bot</p><p>" + "with " + enemy.health + " hit points and wielding " + enemy.weapon + "</p>");
 }
+
 //fight button event listener
 $("#fight").click( () =>{
   // console.log("you clicked to fight");
@@ -113,20 +72,20 @@ function enemyAttack () {
 
  //this function is the battle and is called by the fight button event listener. after each round it calls the function to check if anyone's health is below zero.
 function battle() {
-  // console.log("player one: ", player1);
-  // console.log("enemy: ", enemy);
-  // console.log("player one health: ", player1.health);
-  // console.log("enemy health: ", enemy.health);
+  console.log("player one: ", player1);
+  console.log("enemy: ", enemy);
+  console.log("player one health: ", player1.health);
+  console.log("enemy health: ", enemy.health);
   let enemyAttackDamage = enemyAttack();
   let playerAttackDamage = playerAttack();
-  // console.log("player attacks for ", playerAttackDamage);
-  // console.log("enemy attacks for ", enemyAttackDamage);
+  console.log("player attacks for ", playerAttackDamage);
+  console.log("enemy attacks for ", enemyAttackDamage);
   player1.health = player1.health - enemyAttackDamage;
   enemy.health = enemy.health - playerAttackDamage;
-  // console.log("player health after round: ", player1.health);
-  // console.log("enemy health after round: ", enemy.health);
-  $("#battlePlayerStats").text("enemy hits player for " + enemyAttackDamage + "hit points! After this round, the player has " + player1.health + " hit points!");
-  $("#battleEnemyStats").text("player hits enemy for " + playerAttackDamage + "hit points! After this round the enemy has " + enemy.health + " hit points!");
+  console.log("player health after round: ", player1.health);
+  console.log("enemy health after round: ", enemy.health);
+  $("#battlePlayerStats").text("enemy hits player for " + enemyAttackDamage + " hit points with " + enemy.weapon +"! After this round, the player has " + player1.health + " hit points!");
+  $("#battleEnemyStats").text("player hits enemy for " + playerAttackDamage + " hit points! After this round the enemy has " + enemy.health + " hit points!");
   gameOverCheck();
 }
 
--- scripts/robots.js
+++ scripts/robots.js
@@ -7,75 +7,69 @@ let BattleBot = {};
 
 //base for a BattleBot class - can get it returnining the inherited name of the class but not the weapon.
 BattleBot.BotClass = function() {
-  this.name = "Vanilla";
-  this.weapon = "bad breath";
-  this.healthBonus = 0;
-  this.attackBonus = 0;
-  this.toString = function(){
-    return this.class;
-  };
+  this.name = "";
+  this.weapon = "";
+  this.health = Math.floor(Math.random() * 20 + 90);
+  this.attack = Math.floor(Math.random() * 10 + 20);
 };
 
 //Bear Class Bots
 BattleBot.BearClass = function() {
- this.healthBonus = this.healthBonus - 20;
- this.attackBonus = this.attackBonus + 10;
+  this.health = this.health - 20;
+   this.attack = this.attack + 10;
 };
 BattleBot.BearClass.prototype = new BattleBot.BotClass();
 
 //two types in the bear class
 BattleBot.BrownBear = function() {
- this.name = "Brownie";
- this.weapon = "claws";
- // this.toString = function(){
- //    return this.weapon;
- //  };
+  this.className = "Brown Bear";
+  this.weapon = "claws";
 };
 BattleBot.BrownBear.prototype = new BattleBot.BearClass();
 
 BattleBot.PolarBear = function() {
- this.name = "Paul";
- this.weapon = "fangs";
+  this.className = "Polar Bear";
+  this.weapon = "fangs";
 };
 BattleBot.PolarBear.prototype = new BattleBot.BearClass();
 
 //Humanoid Class Bots
 BattleBot.HumanoidClass = function() {
- this.healthBonus = this.healthBonus + 0;
- this.attackBonus = this.attackBonus + 0;
+ this.health = this.health + 0;
+ this.attack = this.attack + 0;
 };
-BattleBot.BearClass.prototype = new BattleBot.BotClass();
+BattleBot.HumanoidClass.prototype = new BattleBot.BotClass();
 
 //two types in the Humanoid class
 BattleBot.Android = function() {
- this.name = "Bob the Android";
- this.weapon = "Hammer";
+  this.className = "Android";
+  this.weapon = "Hammer";
 };
 BattleBot.Android.prototype = new BattleBot.HumanoidClass();
 
 BattleBot.Elf = function() {
- this.name = "Legolas the Elf";
- this.weapon = "Dagger";
+  this.className = "Elf";
+  this.weapon = "Dagger";
 };
 BattleBot.Elf.prototype = new BattleBot.HumanoidClass();
 
 //Spider Class Bots
 BattleBot.SpiderClass = function() {
- this.healthBonus = this.healthBonus + 20;
- this.attackBonus = this.attackBonus - 10;
+  this.health = this.health + 20;
+  this.attack = this.attack - 10;
 };
 BattleBot.SpiderClass.prototype = new BattleBot.BotClass();
 
 //two types in the spider class
 BattleBot.BlackWidow = function() {
- this.name = "Charlotte";
- this.weapon = "acid";
+  this.className = "Black Widow";
+  this.weapon = "acid";
 };
 BattleBot.BlackWidow.prototype = new BattleBot.SpiderClass();
 
 BattleBot.BrownRecluse = function() {
- this.name = "Sylvia the Spider";
- this.weapon = "web-suffocation";
+  this.className = "Brown Recluse";
+  this.weapon = "web-suffocation";
 };
 BattleBot.BrownRecluse.prototype = new BattleBot.SpiderClass();
 
